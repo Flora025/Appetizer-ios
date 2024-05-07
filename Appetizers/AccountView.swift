1790f24f6746164a1ebe3effb34688ce207777e6
Flora025 <2365349265@qq.com>
2024-05-07 15:18:29 -0400

    feat: create basic tab view

---
+++ Appetizers/AccountView.swift
@@ -0,0 +1,23 @@
+//
+//  AccountView.swift
+//  Appetizers
+//
+//  Created by zhanghan on 5/7/24.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        NavigationView {
+            Text("Account View")
+                .navigationTitle("🍙 Account")
+        }
+    }
+}
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
+}
---
+++ Appetizers/OrderView.swift
@@ -0,0 +1,23 @@
+//
+//  OrderView.swift
+//  Appetizers
+//
+//  Created by zhanghan on 5/7/24.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        NavigationView {
+            Text("Order View")
+                .navigationTitle("📄 Order")
+        }
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView()
+    }
+}
