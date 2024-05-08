6f392d4fe3ae306bd816455159725bd6ae5b6503
Flora025 <2365349265@qq.com>
2024-05-07 21:10:01 -0400

    feat: appetizer list view

---
+++ Appetizers/Appetizer.swift
@@ -0,0 +1,36 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by zhanghan on 5/7/24.
+//
+
+import Foundation
+
+struct Appetizer: Decodable, Identifiable {
+    let id: Int // unique identifier
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    
+    // nutritional info
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+// helper struct for http request
+struct AppetizerRequest {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(
+        id: 0001, name: "Test Appetizer",
+        description: "This is the description", price: 9.99,
+        imageURL: "", calories: 99,
+        protein: 101, carbs: 999)
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
